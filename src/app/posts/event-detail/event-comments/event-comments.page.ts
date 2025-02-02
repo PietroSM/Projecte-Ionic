5984d7d159fc11464714f15f147dba0ec5b5bfc1
PietroSM <pedrosalamora@gmail.com>
2025-02-02 11:50:32 +0100

    Commit_02_02_2025

--- src/app/posts/event-detail/event-comments/event-comments.page.ts
+++ src/app/posts/event-detail/event-comments/event-comments.page.ts
@@ -1,20 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonGrid, IonRow, IonCol, IonAvatar, IonFabButton, IonFab, IonIcon } from '@ionic/angular/standalone';
+import { EventDetailPage } from '../event-detail.page';
+import { CommentInput, Comments } from 'src/app/interfaces/my-event';
+import { EventsService } from 'src/app/services/events.service';
+import { Dialog } from '@capacitor/dialog';
+
 
 @Component({
   selector: 'app-event-comments',
   templateUrl: './event-comments.page.html',
   styleUrls: ['./event-comments.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonFab, IonFabButton, IonAvatar, IonCol, IonRow, IonGrid, IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class EventCommentsPage implements OnInit {
+export class EventCommentsPage{
 
-  constructor() { }
+  event = inject(EventDetailPage).event;
+  comments = signal<Comments[]>([]);
+  #eventsService = inject(EventsService);
 
-  ngOnInit() {
+
+  constructor(){
+    if(this.event()){
+      this.#eventsService.getComments(this.event()!.id)
+      .subscribe({
+        next: (comments) => this.comments.set(comments),
+        error: (error) => console.log(error)
+      });
+    }
+  }
+
+
+  async addCommment() {
+    const result = await Dialog.prompt({
+      title: 'New Comment',
+      message: '',
+    });
+
+    if(!result.cancelled && result.value !== ""){
+      const newComment : CommentInput = {
+        comment: result.value
+      };
+
+      this.#eventsService.postComment(this.event()!.id, newComment)
+      .subscribe({
+        next: () => {
+          this.#eventsService.getComments(this.event()!.id)
+          .subscribe({
+            next: (comments) => this.comments.set(comments),
+            error: (error) => console.log(error),
+          });
+        },
+        error: (error) => console.log(error)
+      });
+
+    }
   }
 
 }
